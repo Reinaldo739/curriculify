283ef4d9a315e7ab5c96d5be78394f65cb60dffd
leonardols10 <61248545+leonardols10@users.noreply.github.com>
2023-11-14 18:17:38 -0300

    Add back e front

---
+++ CurriculifyBack/curriculify/curriculify/src/main/java/com/curriculify/CurriculifyApplication.java
@@ -0,0 +1,13 @@
+package com.curriculify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculifyApplication.class, args);
+	}
+
+}
---
+++ CurriculifyBack/curriculify/curriculify/src/test/java/com/curriculify/CurriculifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curriculify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
