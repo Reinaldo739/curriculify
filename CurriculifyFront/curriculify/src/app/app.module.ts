bad78ead11359f31f8f8e1a09dea9a66513256f5
leonardols10 <leonardosilvalima40@gmail.com>
2023-11-19 21:24:13 -0300

    componentes

--- CurriculifyFront/curriculify/src/app/app.module.ts
+++ CurriculifyFront/curriculify/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePipe } from './pages/home.pipe';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CurriculoComponent } from './pages/curriculo/curriculo.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePipe,
+    HomeComponent,
+    LoginComponent,
+    CurriculoComponent,
+    SobreComponent,
+    MenuComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule
