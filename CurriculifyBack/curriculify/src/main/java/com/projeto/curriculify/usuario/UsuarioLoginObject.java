5f0e1be0a864435c9e1dc6c5d2a6b1d53cdc70fa
Gabriel Armando Cegantini - 942121170 <942121170@ulife.com.br>
2023-12-06 20:43:57 -0300

    funcao logar

--- CurriculifyBack/curriculify/src/main/java/com/projeto/curriculify/usuario/UsuarioLoginObject.java
+++ CurriculifyBack/curriculify/src/main/java/com/projeto/curriculify/usuario/UsuarioLoginObject.java
@@ -1,17 +1,17 @@
 package com.projeto.curriculify.usuario;
 
 public class UsuarioLoginObject {
-	private String login;
+	private String email;
 	private String senha;
 	
 	UsuarioLoginObject(){}
 	
-	public String getLogin() {
-		return login;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setLogin(String login) {
-		this.login = login;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getSenha() {
